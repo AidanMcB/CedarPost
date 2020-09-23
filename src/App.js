a7620a999b807e0ab5d966b1f13c67c25b579fb2
Aidan McBride <aidankmcbride@gmail.com>
2020-09-23 19:11:05 -0400

    add header tag in layouts, adds homepage route and component in App.js

--- src/App.js
+++ src/App.js
@@ -3,11 +3,16 @@ import './App.css';
 import {BrowserRouter, Route, Path} from 'react-router-dom'
 // Components
 import About from './components/About/About'
+import Header from './layouts/Header'
+import HomePage from './components/Home/HomePage'
 function App() {
 
   const [state,setState] = useState(0)
+
   return (
    <BrowserRouter>
+      <Header />
+      <Route exact path="/" component={HomePage} />
       <Route exact path="/about" component={About} />
    </BrowserRouter>
   );
