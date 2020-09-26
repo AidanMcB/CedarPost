034d31e769c02bbf3aadd0584466cf8d653f3a40
Aidan McBride <aidankmcbride@gmail.com>
2020-09-26 13:08:29 -0400

    hanging baskets

---
+++ src/components/Hanging Baskets/HanginBaskets.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function HangingBaskets() {
+
+    return (
+        <div>
+            <h1>Hanging Baskets</h1>
+        </div>
+    )
+}
--- src/components/Home/HomePage.js
+++ src/components/Home/HomePage.js
@@ -1,8 +1,10 @@
 import React from 'react';
 
 export default function HomePage() {
-    
-    return(
-        <div>Hello</div>
+
+    return(      
+        <div>
+            <h1>Cedar Post Farm Market and Garden Center</h1>
+        </div>
     )
 }
